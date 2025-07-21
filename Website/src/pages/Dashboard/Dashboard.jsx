4b00dacd5f244e5b1be4d92ef6a38a81e789ceff
mspatell <mspatel4ca@gmail.com>
2025-07-21 18:05:23 -0400

    minor bug fix

--- Website/src/pages/Dashboard/Dashboard.jsx
+++ Website/src/pages/Dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import InfoCard from '../../components/InfoCard/infoCard';
 import './Dashboard.css';
 
-const serverUrl = process.env.REACT_APP_SERVER_URL;
+const serverUrl = process.env.REACT_APP_LOCAL_SERVER_URL;
 
 function Dashboard(props) {
     const [image, setImage] = useState(null);
@@ -79,7 +79,7 @@ function Dashboard(props) {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
                 },
-                body: {}
+                body: JSON.stringify({})
             });
 
             const recognitionResult = await recognitionResponse.json();
